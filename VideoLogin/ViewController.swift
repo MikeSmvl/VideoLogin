fde3843a8a1860a1476a35ab1e18bfabc29db887
MikeSmvl <Mitchel@mikaels-macbook-pro.local>
2016-01-02 17:51:54 -0500

    First Commit

---
+++ VideoLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoLogin
+//
+//  Created by Mikael Samvelian on 2016-01-02.
+//  Copyright © 2016 MeetUp Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
